67c828e7c1faa5a4e3330d5a2f8b5b7dedda946f
Rodrigo Gonçalves <rodrigosg2000@gmail.com>
2019-08-07 23:22:32 -0300

    Add auth service

--- auth/src/main/java/academy/devdojo/youtube/auth/security/config/SecurityCredentialsConfig.java
+++ auth/src/main/java/academy/devdojo/youtube/auth/security/config/SecurityCredentialsConfig.java
@@ -1,15 +1,16 @@
 package academy.devdojo.youtube.auth.security.config;
 
+import academy.devdojo.youtube.auth.security.filter.JwtUsernameAndPasswordAuthenticationFilter;
 import academy.devdojo.youtube.core.config.JwtConfiguration;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 
 import javax.servlet.http.HttpServletResponse;
@@ -33,7 +34,7 @@ public class SecurityCredentialsConfig extends WebSecurityConfigurerAdapter {
             .and()
                 .exceptionHandling().authenticationEntryPoint((req, res, e) -> res.sendError(HttpServletResponse.SC_UNAUTHORIZED))
             .and()
-                .addFilter(new UsernamePasswordAuthenticationFilter())
+                .addFilter(new JwtUsernameAndPasswordAuthenticationFilter(authenticationManager(), jwtConfiguration))
             .authorizeRequests()
                 .antMatchers(jwtConfiguration.getLoginUrl()).permitAll()
                 .antMatchers("/course/admin/**").hasRole("ADMIN")
--- auth/src/test/java/academy/devdojo/youtube/auth/AuthApplicationTests.java
+++ auth/src/test/java/academy/devdojo/youtube/auth/AuthApplicationTests.java
@@ -3,6 +3,7 @@ package academy.devdojo.youtube.auth;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
@@ -11,6 +12,7 @@ public class AuthApplicationTests {
 
     @Test
     public void contextLoads() {
+        System.out.println(new BCryptPasswordEncoder().encode("devdojo"));
     }
 
 }
