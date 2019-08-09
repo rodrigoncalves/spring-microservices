9580224fc4ca6a2abd45130ea1e7dfe0545e2d2c
Rodrigo Gonçalves <rodrigosg2000@gmail.com>
2019-08-09 19:53:01 -0300

    Spring Boot Microservices 07 - Authorization Filter Pt 01

---
+++ gateway/src/main/java/academy/devdojo/youtube/gateway/security/config/SecurityConfig.java
@@ -0,0 +1,4 @@
+package academy.devdojo.youtube.gateway.security.config;
+
+public class SecurityConfig {
+}
---
+++ token/src/main/java/academy/devdojo/youtube/security/filter/JwtTokenAuthorizationFilter.java
@@ -0,0 +1,55 @@
+package academy.devdojo.youtube.security.filter;
+
+import academy.devdojo.youtube.core.config.JwtConfiguration;
+import academy.devdojo.youtube.security.token.converter.TokenConverter;
+import academy.devdojo.youtube.security.util.SecurityContextUtil;
+import com.nimbusds.jwt.SignedJWT;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.lang.NonNull;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static org.apache.commons.lang3.StringUtils.equalsIgnoreCase;
+
+@Slf4j
+@RequiredArgsConstructor
+public class JwtTokenAuthorizationFilter extends OncePerRequestFilter {
+
+    private final JwtConfiguration jwtConfiguration;
+    private final TokenConverter tokenConverter;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain chain) throws ServletException, IOException {
+        String header = request.getHeader(jwtConfiguration.getHeader().getName());
+
+        String prefix = jwtConfiguration.getHeader().getPrefix();
+        if (header == null || !header.startsWith(prefix)) {
+            chain.doFilter(request, response);
+            return;
+        }
+
+        String token = header.replace(prefix, "").trim();
+        boolean isEncrypted = equalsIgnoreCase("signed", jwtConfiguration.getType());
+        SecurityContextUtil.setSecurityContext(isEncrypted ? validate(token) : decryptAndValidate(token));
+
+        chain.doFilter(request, response);
+    }
+
+    private SignedJWT decryptAndValidate(String encryptedToken) {
+        String signedToken = tokenConverter.decryptToken(encryptedToken);
+        return validate(signedToken);
+    }
+
+    @SneakyThrows
+    private SignedJWT validate(String signedToken) {
+        tokenConverter.validateTokenSignature(signedToken);
+        return SignedJWT.parse(signedToken);
+    }
+}
